eec794ce1a8293c43a9760c9ccf22a8ad8a4b3c3
yupher <hamzabouhadjila@gmail.com>
2021-01-14 21:31:20 +0100

    prepare to dploy

--- sharrserver/app.js
+++ sharrserver/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
+const path = require('path')
 const cors = require('cors')
 const helmet = require('helmet')
 const mongoose = require('mongoose')
+require('dotenv').config()
+const {db} = require('./config')
 
 const app = express()
 app.use(helmet())
-mongoose.connect('mongodb://localhost:27017/sharingapp',{useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false,useCreateIndex: true,})
+mongoose.connect(db,{useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false,useCreateIndex: true,})
   .then(()=>console.log('mongodb satrted.....'))
   .catch(e=>console.log(e))
 app.use(fileUpload({
@@ -17,12 +20,18 @@ app.use(fileUpload({
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-global.__basedir = __dirname;
+
 
 
 
 app.use('/api/files', require('./routes/api/files'))
 app.use('/', require('./routes/redirect'))
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('../sharrclient/build'))
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__basedir, 'sharrclient', 'build', 'index.html'))
+  })
+}
 
-
-app.listen(5000,()=>console.log('server started...'))
+let PORT = 5000 || process.env.PORT
+app.listen(PORT,()=>console.log('server started...'))
---
+++ sharrserver/config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  db : `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.aia33.mongodb.net/sharr?retryWrites=true&w=majority` 
+}
